e1f8001dc415a3a6caec449e5d086d1424241fde
Rafael COPPE <rafael.coppe.jr@gmail.com>
2025-06-26 15:26:55 +0200

    reworked api pipeline to include doc

--- mongo-init.js
+++ mongo-init.js
@@ -19,13 +19,13 @@ db.apis.insertMany([
     description: 'Cette API est le point d’entrée à la base de données des équipements sportifs et des lieux de pratiques du ministère chargé des Sports. Celle-ci est mise à jour quotidiennement grâce à nos équipes d\'enquêteurs et des déclarations réalisées par les propriétaires. Vous y retrouverez tous les éléments liés à l’équipement (géolocalisation, type d’équipement, dimension, tribunes, aménagements, accessibilité, utilisateurs …) mais aussi les éléments liés à son installation (adresse, propriétaire, erp, type d’établissement …). Chaque équipement possède un code référentiel national unique.',
     link: '/api/explore/v2.1/catalog/datasets/data-es/records',
     api_key: null,
-    doc: 'DataEs.json'
+    doc: 'dataes.json'
   },
   {
     name: 'Basilic',
     description: 'Cette base géocodée des sites, lieux et équipements culturels est conçue et administrée par le département des études, de la prospective, des statistiques et de la documentation du ministère de la Culture. Elle alimente notamment la base permanente des équipements de l\'Insee et l\'Atlas Culture des territoires.',
     link: '/api/explore/v2.1/catalog/datasets/base-des-lieux-et-des-equipements-culturels/records',
     api_key: null,
-    doc: 'Basilic.json'
+    doc: 'basilic.json'
   },
 ]);
